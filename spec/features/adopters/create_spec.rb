32a62acad93d948b769852efe031ca522a578a6c
Gavin Carew <gjcarew@gmail.com>
2022-09-02 08:02:57 -0700

    Create tests for creating an application (adopter)

---
+++ spec/features/adopters/create_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'New application' do
+  describe 'As a visitor' do
+    describe 'when I visit the pet index page' do
+      it 'I see a link to Start an Application' do
+        visit '/pets'
+        click_on 'Start an Application'
+        expect(current_path).to eq('/adopters/new')
+      end
+
+      it 'the application has a form' do
+        visit '/adopters/new'
+        expect(page).to have_content('New Application for Adoption')
+        expect(find('form')).to have_content('Name')
+        expect(find('form')).to have_content('Address')
+        expect(find('form')).to have_content('City')
+        expect(find('form')).to have_content('State')
+        expect(find('form')).to have_content('Zip Code')
+      end
+
+      it 'When I fill in the application, I see my info on the application show page' do
+        visit '/adopters/new'
+        fill_in 'Name', with: 'Franklin D. Roosevelt'
+        fill_in 'Address', with: '1600 Pennsylvania Avenue'
+        fill_in 'City', with: 'Washington'
+        fill_in 'State', with: 'District of Columbia'
+        fill_in 'Zip Code', with: '20500'
+        click_button 'Submit'
+        new_id = Adopter.last.id
+        expect(current_path).to eq("/adopters/#{new_id}")
+        expect(page).to have_content('Franklin D. Roosevelt')
+        expect(page).to have_content('1600 Pennsylvania Avenue')
+        expect(page).to have_content('Washington')
+        expect(page).to have_content('District of Columbia')
+        expect(page).to have_content('20500')
+        expect(page).to have_content('In Progress')
+      end
+    end
+  end
+end
