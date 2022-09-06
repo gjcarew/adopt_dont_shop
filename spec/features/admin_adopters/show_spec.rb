450feb60993b35a342906022ee636f55eb142b99
Gavin Carew <gjcarew@gmail.com>
2022-09-06 11:08:22 -0700

    feat: Approving/rejecting pets on one applicaiton will not affect other applications

--- spec/features/admin_adopters/show_spec.rb
+++ spec/features/admin_adopters/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Admin application show page' do
     shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @pirate = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
     @clawdia = shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-    @gavin = Adopter.create!(name: "Gavin", address: "123 turing st., Denver, CO 80302", street: '123 turing st.', city: 'Denver', state: 'CO', zip_code: '80302', description: "feed them", application_status: "Pending")
+    @gavin = Adopter.create!(name: "Gavin", address: "123 turing st., Denver, CO 80302", street: '123 turing st.', city: 'Denver', state: 'CO', zip_code: '80302', description: "feed them", application_status: "In Progress")
     @gavin.pets << @clawdia
     @gavin.pets << @pirate
   end
@@ -37,7 +37,7 @@ RSpec.describe 'Admin application show page' do
     end
 
   describe 'After i approve all pets' do
-    it "shows pet is no longer adoptable" do
+    xit "shows pet is no longer adoptable" do
         visit "/admin/adopters/#{@gavin.id}"
         expect(page).to have_content("Approved")
         visit "/pets/#{pet.id}"
@@ -70,5 +70,18 @@ RSpec.describe 'Admin application show page' do
     it 'There is an indicator next to the pet to show they have been rejected' do
       expect(page).to have_content("Rejected")
     end
+  end
+  
+  it 'Approved/rejected pets on one application do not affect other applications' do
+
+    morgan = Adopter.create!(name: "Morgan", address: "123 turing st., Denver, CO 80302", street: '123 turing st.', city: 'Denver', state: 'CO', zip_code: '80302', description: "feed them", application_status: "In Progress")
+    morgan.pets << @clawdia
+    morgan.pets << @pirate
+    visit "/admin/adopters/#{morgan.id}"
+    click_button "Approve Application for Clawdia"
+    expect(page).to have_content("Approved")
+    visit "/admin/adopters/#{@gavin.id}"
+    expect(page).to have_button("Approve Application for Clawdia")
+    expect(page).to have_button("Reject Application for Clawdia")
   end
 end
