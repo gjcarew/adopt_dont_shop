8b38ec161c6605bfd7bff8f7d6ab23416de0b352
Shawn Lee <Shawnl93@hotmail.com>
2022-09-05 21:41:59 -0600

    created admin_shelter show page

--- app/controllers/admin_shelters_controller.rb
+++ app/controllers/admin_shelters_controller.rb
@@ -2,4 +2,8 @@ class AdminSheltersController < ApplicationController
   def index
     @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY shelters.name DESC")
   end
+
+  def show
+    @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY shelters.name DESC")
+  end
 end
